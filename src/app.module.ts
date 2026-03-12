4862f43e86cb9c10c8409204281b39ea51c00ad6
schirila <chiriladev@gmail.com>
2026-03-12 21:17:21 +0100

    export PrismaService and remove PrismaModule from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [],
   controllers: [],
   providers: [],
 })
